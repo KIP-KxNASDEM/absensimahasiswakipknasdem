a7388d1ed912b2b641de47a3fcdd44ecd07297ca
KIP-KxNASDEM <nasdemsidoarjo25@gmail.com>
2026-08-22 21:27:57 +0700

    Update admin-reports.js

--- js/admin-reports.js
+++ js/admin-reports.js
@@ -200,8 +200,12 @@ const norm = v => String(v ?? '').trim().toLowerCase();
             this.rawLeaves = arr(l);
             this.rawIzin = arr(i);
             
-            this.rawJournals = j?.data || [];
-            this.journalData = j.data || [];
+           this.rawJournals = Array.isArray(j) 
+               ? j 
+               : (j?.data || []);
+            this.journalData = this.rawJournals;
+        console.log("J OBJECT:", j);
+        console.log("FINAL JOURNAL:", this.journalData);
         console.log(
             "SET JOURNAL DATA:",
             this.journalData
